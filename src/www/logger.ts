4c04d8c7052f57965eb0dcf493599abc739adfd2
Stéphane SL. LEONARD <stephane.leonard@iotize.com>
2020-06-10 18:10:50 +0200

    fix: fix logger prefix to @iotize/device-com-wifi.cordova instead of @iotize/device-com-mqtt.cordova

--- src/www/logger.ts
+++ src/www/logger.ts
@@ -1,3 +1,3 @@
 import { createDebugger } from '@iotize/common/debug';
 
-export const debug = createDebugger(`@iotize/device-com-mqtt.cordova`);
+export const debug = createDebugger(`@iotize/device-com-wifi.cordova`);
